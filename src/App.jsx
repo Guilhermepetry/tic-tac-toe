c547f1f5ca53193f4311a3d143e37636df6b5739
Guilhermepetry <guilhermepetryoficial@Hotmail.com>
2023-04-01 16:33:30 -0300

    :sparkles: Create Logo object

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,11 @@
 import "./App.css";
 import About from "./objects/About";
+import Logo from "./objects/Logo";
 
 function App() {
   return (
     <>
+      <Logo />
       <About />
     </>
   );
---
+++ src/objects/Logo/index.jsx
@@ -0,0 +1,7 @@
+import imgUrl from "../../assets/images/collabcode.png";
+
+function Logo() {
+  return <img src={imgUrl} alt="Logo da Collabcode" />;
+}
+
+export default Logo;
