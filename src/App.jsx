d65c946ad811f1698f3f968bd23e728c22f1c46e
Guilhermepetry <guilhermepetryoficial@Hotmail.com>
2023-04-01 17:00:12 -0300

    :sparkles: Create Menu object

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,14 @@
 import "./App.css";
 import About from "./objects/About";
 import Logo from "./objects/Logo";
+import Menu from "./objects/Menu";
 
 function App() {
   return (
     <>
       <Logo />
       <About />
+      <Menu />
     </>
   );
 }
---
+++ src/objects/Menu/index.jsx
@@ -0,0 +1,11 @@
+import "./style.css";
+
+function Menu() {
+  return (
+    <a className="menu" href="#menu">
+      <span className="center">Menu</span>
+    </a>
+  );
+}
+
+export default Menu;
